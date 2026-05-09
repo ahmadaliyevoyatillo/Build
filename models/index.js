a6c022eaa6c3aa08a6d78793c0448980f32089ff
ahmadaliyevoyatillo <oyatilloahmadaliyev10@gmail.com>
2026-05-09 12:08:33 +0500

    fix ssl

--- models/index.js
+++ models/index.js
@@ -3,9 +3,15 @@ require("dotenv").config()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     logging: false
 })
-
 const db = {}
 db.sequelize = sequelize
 db.Sequelize = Sequelize
